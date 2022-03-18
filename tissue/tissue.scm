2fabe47a52ee71692648eb22218c696e1e213673
Arun Isaac <arunisaac@systemreboot.net>
2022-03-18 16:12:36 +0530

    tissue: Introduce aliases in configuration.
    * tissue/tissue.scm (<tissue-configuration>)[aliases]: New field.
    * tissue/tissue.scm (tissue-configuration): Accept aliases as an
    argument.

--- tissue/tissue.scm
+++ tissue/tissue.scm
@@ -21,19 +21,25 @@
   #:export (tissue-configuration
             tissue-configuration?
             tissue-configuration-project
+            tissue-configuration-aliases
             tissue-configuration-web-css
             tissue-configuration-web-tags-path))
 
 (define-record-type <tissue-configuration>
-  (make-tissue-configuration project web-css web-tags-path)
+  (make-tissue-configuration project aliases web-css web-tags-path)
   tissue-configuration?
   (project tissue-configuration-project)
+  (aliases tissue-configuration-aliases)
   (web-css tissue-configuration-web-css)
   (web-tags-path tissue-configuration-web-tags-path))
 
-(define* (tissue-configuration #:key project web-css (web-tags-path "/tags"))
+(define* (tissue-configuration #:key project (aliases '()) web-css (web-tags-path "/tags"))
   "PROJECT is the name of the project. It is used in the title of the
 generated web pages, among other places.
+
+ALIASES is a list of aliases used to refer to authors in the
+repository. Each element is in turn a list of aliases an author goes
+by, the first of which is the canonical name of that author.
 
 WEB-CSS is the path to a CSS stylesheet. It is relative to the
 document root and must begin with a /. If it is #f, no stylesheet is
@@ -42,4 +48,4 @@ used in the generated web pages.
 WEB-TAGS-PATH is the path relative to the document root where the
 per-tag issue listings are put. It must begin with a /. If it is #f,
 per-tag issue listings are not generated."
-  (make-tissue-configuration project web-css web-tags-path))
+  (make-tissue-configuration project aliases web-css web-tags-path))
