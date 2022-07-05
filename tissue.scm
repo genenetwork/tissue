ce731958fa24752850ad669a50c04fe2a1348aa5
Arun Isaac <arunisaac@systemreboot.net>
2022-07-06 01:46:57 +0530

    website: Add home page.
    * website/index.skb: New file.
    * tissue.scm (#:web-files): Add index.html.

--- tissue.scm
+++ tissue.scm
@@ -4,7 +4,9 @@
                             (indexed-document (cut read-gemtext-issue filename)
                                               (string-append "/" (replace-extension filename "html"))))
                           (gemtext-files-in-directory "issues"))
- #:web-files (filter-map (lambda (filename)
-                           (file (replace-extension filename "html")
-                                 (gemtext-exporter filename)))
-                         (gemtext-files-in-directory "issues")))
+ #:web-files (cons (file "index.html"
+                         (skribe-exporter "website/index.skb"))
+                   (filter-map (lambda (filename)
+                                 (file (replace-extension filename "html")
+                                       (gemtext-exporter filename)))
+                               (gemtext-files-in-directory "issues"))))
