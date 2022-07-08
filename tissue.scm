526f422ce50823f7d78be1857f4538f600ffafd9
Arun Isaac <arunisaac@systemreboot.net>
2022-07-09 00:27:26 +0530

    bin: Make indexed documents a list of document objects.
    This simplifies the interface obviating the need for an
    <indexed-document> type.
    
    * bin/tissue: Do not import (srfi srfi-9).
    (<indexed-document>): Delete type.
    (index): Expect indexed documents to be a list of document objects.
    * tissue.scm (#:indexed-documents): Pass in a list of document
    objects, instead of a list of <indexed-document> objects.

--- tissue.scm
+++ tissue.scm
@@ -1,8 +1,9 @@
 (tissue-configuration
  #:project "tissue"
  #:indexed-documents (map (lambda (filename)
-                            (indexed-document (cut read-gemtext-issue filename)
-                                              (string-append "/" (string-remove-suffix ".gmi" filename))))
+                            (slot-set (read-gemtext-issue filename)
+                                      'web-uri
+                                      (string-append "/" (string-remove-suffix ".gmi" filename))))
                           (gemtext-files-in-directory "issues"))
  #:web-files (cons (file "index.html"
                          (skribe-exporter "website/index.skb"))
