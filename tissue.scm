84abb60abc0fdf509849a17e34d501bec62818e0
Arun Isaac <arunisaac@systemreboot.net>
2022-09-19 02:15:48 +0530

    tissue.scm: Add CSS.
    * website/style.css: New file.
    * tissue.scm (#:web-files): Copy CSS to website.
    (#:web-css): Use it.

--- tissue.scm
+++ tissue.scm
@@ -11,17 +11,20 @@
                                               (string-append "https://git.systemreboot.net/tissue/commit/?id="
                                                              (commit-hash commit))))
                                   (commits-in-current-repository)))
- #:web-files (cons (file "index.html"
-                         (skribe-exporter "website/index.skb"))
-                   (append (map (lambda (font-file)
-                                  (file (string-append "fonts/" font-file)
-                                        (copier (string-append (getenv "GUIX_ENVIRONMENT")
-                                                               "/share/fonts/web/" font-file))))
-                                (list "IBMPlexSans-Regular-Latin1.woff2"
-                                      "IBMPlexSans-Bold-Latin1.woff2"
-                                      "IBMPlexMono-Regular-Latin1.woff2"
-                                      "IBMPlexMono-Bold-Latin1.woff2"))
-                           (filter-map (lambda (filename)
-                                         (file (replace-extension filename "html")
-                                               (gemtext-exporter filename)))
-                                       (gemtext-files-in-directory "issues")))))
+ #:web-css "/style.css"
+ #:web-files (cons* (file "index.html"
+                          (skribe-exporter "website/index.skb"))
+                    (file "style.css"
+                          (copier "website/style.css"))
+                    (append (map (lambda (font-file)
+                                   (file (string-append "fonts/" font-file)
+                                         (copier (string-append (getenv "GUIX_ENVIRONMENT")
+                                                                "/share/fonts/web/" font-file))))
+                                 (list "IBMPlexSans-Regular-Latin1.woff2"
+                                       "IBMPlexSans-Bold-Latin1.woff2"
+                                       "IBMPlexMono-Regular-Latin1.woff2"
+                                       "IBMPlexMono-Bold-Latin1.woff2"))
+                            (filter-map (lambda (filename)
+                                          (file (replace-extension filename "html")
+                                                (gemtext-exporter filename)))
+                                        (gemtext-files-in-directory "issues")))))
