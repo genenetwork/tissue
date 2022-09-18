ba2cde414865b86bd062591109797ac1738fb1d8
Arun Isaac <arunisaac@systemreboot.net>
2022-09-19 02:13:21 +0530

    tissue.scm: Copy IBM Plex fonts to built website.
    * tissue.scm (#:web-files): Copy IBM Plex fonts.

--- tissue.scm
+++ tissue.scm
@@ -13,7 +13,15 @@
                                   (commits-in-current-repository)))
  #:web-files (cons (file "index.html"
                          (skribe-exporter "website/index.skb"))
-                   (filter-map (lambda (filename)
-                                 (file (replace-extension filename "html")
-                                       (gemtext-exporter filename)))
-                               (gemtext-files-in-directory "issues"))))
+                   (append (map (lambda (font-file)
+                                  (file (string-append "fonts/" font-file)
+                                        (copier (string-append (getenv "GUIX_ENVIRONMENT")
+                                                               "/share/fonts/web/" font-file))))
+                                (list "IBMPlexSans-Regular-Latin1.woff2"
+                                      "IBMPlexSans-Bold-Latin1.woff2"
+                                      "IBMPlexMono-Regular-Latin1.woff2"
+                                      "IBMPlexMono-Bold-Latin1.woff2"))
+                           (filter-map (lambda (filename)
+                                         (file (replace-extension filename "html")
+                                               (gemtext-exporter filename)))
+                                       (gemtext-files-in-directory "issues")))))
